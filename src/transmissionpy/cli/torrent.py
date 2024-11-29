834f360b558723af0b383716b744b757231822e0
redjax <none@none.com>
2024-11-29 06:49:07 +0000

    Move print_df logic to a function for re-use

--- src/transmissionpy/cli/torrent.py
+++ src/transmissionpy/cli/torrent.py
@@ -16,6 +16,46 @@ import pandas as pd
 
 torrent_app = App(name="torrent", help="Torrent management commands.")
 
+def torrents_to_df(torrents: list[TorrentMetadataIn], dtype_mapping: dict = torrent_df_dtypes_mapping, col_rename_mapping: dict | None = None) -> pd.DataFrame:
+    try:
+        converted_torrents: list[TorrentMetadataIn] = rpc_client.utils.convert_multiple_torrents_to_torrentmetadata(torrents=torrents)
+        torrents_df: pd.DataFrame = rpc_client.utils.convert_torrents_to_df(torrents=converted_torrents)
+    
+        torrents_df = df_utils.convert_df_col_dtypes(df=torrents_df, dtype_mapping=dtype_mapping)
+        
+        if col_rename_mapping:
+            try:
+                torrents_df = df_utils.rename_df_cols(df=torrents_df, col_rename_map=col_rename_mapping)
+            except Exception as exc:
+                msg = f"({type(exc)}) Error renaming columns: {exc}"
+                log.error(msg)
+                log.warning("Continuing without renaming DataFrame columns.")
+        
+        return torrents_df
+
+    except Exception as exc:
+        msg = f"({type(exc)}) Error creating dataframe from torrent list. Details: {exc}"
+        log.error(msg)
+        
+        raise exc
+    
+def df_float_to_percent(df: pd.DataFrame, col_name: str) -> pd.DataFrame:
+    df_copy = df.copy()
+    df_copy[col_name].apply(lambda x: "{:.2f}%".format(round(x * 100, 2)))
+    
+    return df_copy
+
+
+def select_df_cols(df: pd.DataFrame, cols: list[str] | None = None):
+    if not cols:
+        return df
+
+    df_copy = df.copy()
+    df_copy = df_copy[cols]
+    
+    return df_copy
+
+
 @torrent_app.command(name="count")
 def count_torrents(status: t.Annotated[str, Parameter(name="status", show_default=True)] = "all"):
     """Count torrents by status.
@@ -41,6 +81,37 @@ def count_torrents(status: t.Annotated[str, Parameter(name="status", show_defaul
     else:
         log.info(f"Found {count} {status} torrent(s)")
 
+
+def print_torrent_df(torrent_df: pd.DataFrame, dtype_mapping: dict = torrent_df_dtypes_mapping, rename_columns: t.Mapping[str, str] | None = {"id": "torrentId", "name": "torrent", "isFinished": "finished", "isStalled": "stalled", "addedDate": "date added", "activityDate": "last active", "percentDone": "done", "timeDownloading": "download time"}, status: str = "all", max_print_rows: int = 300, df_preview_rows: int = 5,  show_columns: list[str] = ["id", "name", "isFinished", "isStalled", "addedDate", "activityDate", "downloadedEver", "error", "percentDone", "timeDownloading"]):
+    print_df: pd.DataFrame = torrent_df.copy(deep=True)
+    
+    ## Hide pandas index so transmission ID is less confusing
+    print_df = df_utils.hide_df_index(df=print_df)
+    
+    ## Convert datatypes
+    print_df: pd.DataFrame = df_utils.convert_df_col_dtypes(df=print_df, dtype_mapping=dtype_mapping)
+    ## Convert secondsDownloading column to timedelta
+    print_df["timeDownloading"] = print_df["secondsDownloading"].apply(time_utils.convert_seconds_to_timedelta)
+    
+    ## Select subset of columns to display
+    print_df = select_df_cols(df=print_df, cols=show_columns)
+    ## Convert percentDone column to percentage string
+    print_df = df_float_to_percent(df=print_df, col_name="percentDone")
+    
+    ## Rename columns
+    print_df.rename(columns=rename_columns, inplace=True)
+    
+    if status == "all":
+        log.info(f"All torrent count: {print_df.shape[0]}")
+        log.info("Torrents:")
+    else:
+        log.info(f"{status.title()} torrent count: {print_df.shape[0]}")
+        log.info(f"{status.title()} torrents:")
+    
+    with pd.option_context("display.max_rows", max_print_rows or 100):
+        print(print_df.head(df_preview_rows or 5))
+    
+    
 
 @torrent_app.command(name="list")
 def list_torrents(status: t.Annotated[str, Parameter(name="status", show_default=True)] = "all", preview: t.Annotated[int, Parameter(name=["-p", "--preview"])] = 5, limit: t.Annotated[int, Parameter(name=["-l", "--limit"])] = 300):
@@ -69,13 +140,7 @@ def list_torrents(status: t.Annotated[str, Parameter(name="status", show_default
 
         return
     
-    converted_torrents: list[TorrentMetadataIn] = rpc_client.utils.convert_multiple_torrents_to_torrentmetadata(torrents=torrents)
-    torrents_df: pd.DataFrame = rpc_client.utils.convert_torrents_to_df(torrents=converted_torrents)
-    # torrents_df = df_utils.convert_df_datetimes_to_timestamp(df=torrents_df)
-    torrents_df: pd.DataFrame = df_utils.convert_df_col_dtypes(df=torrents_df, dtype_mapping=torrent_df_dtypes_mapping)
-    
-    ## Convert secondsDownloading column to timedelta
-    torrents_df["timeDownloading"] = torrents_df["secondsDownloading"].apply(time_utils.convert_seconds_to_timedelta)
+    torrents_df: pd.DataFrame = torrents_to_df(torrents=torrents)
     
     if isinstance(torrents_df, pd.DataFrame) and torrents_df.empty:
         if status == "all": 
@@ -84,28 +149,11 @@ def list_torrents(status: t.Annotated[str, Parameter(name="status", show_default
             log.warning(f"No {status.title()} torrents found at remote")
         return
     
-    print_df: pd.DataFrame = torrents_df[["id", "name", "isFinished", "isStalled", "addedDate", "activityDate", "downloadedEver", "error", "percentDone", "timeDownloading"]]
-    ## Convert percentDone column to string for displaying to user
-    print_df["percentDone"] = print_df["percentDone"].apply(lambda x: "{:.2f}%".format(round(x * 100, 2)))
+    ## Display torrents dataframe in CLI
+    print_torrent_df(torrent_df=torrents_df, status=status, max_print_rows=limit, df_preview_rows=preview)
     
-    if status == "all":
-        log.info(f"All torrent count: {print_df.shape[0]}")
-        log.info("Torrents:")
-    else:
-        log.info(f"{status.title()} torrent count: {print_df.shape[0]}")
-        log.info(f"{status.title()} torrents:")
-        
-    print_df = print_df.rename(columns={"id": "torrentId"})
-    ## Hide pandas index so transmission ID is less confusing
-    print_df = df_utils.hide_df_index(df=print_df)
-    
-    # with pd.option_context("display.max_columns", 50):
-        ## Hide pandas index so transmission ID is less confusing
-        # print(print_df.head(preview).to_string(index=False))
-    with pd.option_context("display.max_rows", limit):
-        print(print_df.head(preview))
-    
-    # return torrents_df
-    
+    return torrents_df
 
-# @torrent_app.command(name="")
+@torrent_app.command(name=["rm", "remove"])
+def remove_torrent(torrent_id: t.Annotated[int, Parameter(name=["--id"])] | None = None, all: t.Annotated[bool, Parameter(name=["--all", "-a"])] = False):
+    """Remove a torrent, or multiple torrents by state."""
